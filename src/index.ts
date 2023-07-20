b744fcacd692445dc5521c3a6ee46b502b5645ea
Cedrouseroll omondi <omondicedo@gmail.com>
2023-07-20 22:16:02 +0300

    chore: added lib entry file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
+import type { App } from "vue";
 import VueCompareImage from "./components";
+
+// TODO: provide image options
+export default {
+    install: (app: App) => {
+        app.component("VueCompareImage", VueCompareImage)
+    }
+}
 
 export { VueCompareImage };
