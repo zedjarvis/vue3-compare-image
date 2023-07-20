2ff962a4926a80d20ebef91cd1432dbcd794553f
Cedrouseroll omondi <omondicedo@gmail.com>
2023-07-20 22:25:27 +0300

    chore: module packaging config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,27 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'node:path'
+import dts from "vite-plugin-dts"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    dts({})
   ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: "VueCompareImage",
+      fileName: "vue3-compare-image",
+    },
+    rollupOptions: {
+      external: ["Vue"],
+      output: {
+        globals: {
+          vue: "Vue"
+        }
+      }
     }
   }
 })
