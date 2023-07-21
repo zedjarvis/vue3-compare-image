50fe4a15eaf12f8501e560899d043aedeb956436
Cedrouseroll omondi <omondicedo@gmail.com>
2023-07-21 22:33:17 +0300

    vue type declaration

--- env.d.ts
+++ env.d.ts
@@ -1,3 +1,7 @@
 /// <reference types="vite/client" />
 
-declare module "vue3-highlightjs";
+declare module '*.vue' {
+    import type { DefineComponent } from 'vue'
+    const component: DefineComponent<{}, {}, any>
+    export default component
+}
