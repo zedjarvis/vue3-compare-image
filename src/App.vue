b25ee8c1a717c944442c2bf9af53b0942273b399
Cedrouseroll omondi <omondicedo@gmail.com>
2023-07-21 22:34:21 +0300

    add demo

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 // componets
+import VueCompareImage from './components/VueCompareImage.vue';
 </script>
 
 <template>
   <div class="demo-page">
     <h1 style="margin: 0 auto;">
       <a href="https://github.com/zedjarvis/vue3-compare-image" target="_blank">Vue3 Compare Image</a>
+      <VueCompareImage left-image="/favicon.ico" right-image="/favicon.ico" />
     </h1>
   </div>
 </template>
