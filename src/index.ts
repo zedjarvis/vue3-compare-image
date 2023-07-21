a69f10198ffa4b763cfc39aa23102781766ec16e
Cedrouseroll omondi <omondicedo@gmail.com>
2023-07-21 22:32:42 +0300

    feat: component can be installed globally or used as single import

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
+import type { App } from "vue";
 import VueCompareImage from "./components/VueCompareImage.vue";
 
+export default {
+    install: (app: App) => {
+        app.component('VueCompareImage', VueCompareImage);
+    }
+};
 
-export default VueCompareImage ;
+export { VueCompareImage };
