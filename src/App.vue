09cea40023e7a11a33c70d4338645251177be5af
Cedrouseroll omondi <omondicedo@gmail.com>
2023-07-23 11:41:55 +0300

    demo update

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-// componets
+// cassets
+import cat1 from "/src/assets/cat1.jpg";
+import cat2 from "/src/assets/cat2.jpg";
+
 </script>
 
 <template>
   <div class="container" style="max-width: 800px; width: auto; height: auto;">
-    <VueCompareImage left-image="/cat1.jpg" left-image-label="Left Image" right-image="/cat2.jpg" right-image-label="Right Image" vertical slider-line-color="red"/>
+    <VueCompareImage :left-image="cat1" left-image-label="Left Image" :right-image="cat2" right-image-label="Right Image" />
   </div>
 </template>
