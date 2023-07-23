f99d7cce7e8351d2b879b531794be7477be9b417
Cedrouseroll omondi <omondicedo@gmail.com>
2023-07-23 04:15:01 +0300

    refactor: removed component import

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
 <script setup lang="ts">
 // componets
-import VueCompareImage from './components/VueCompareImage.vue';
 </script>
 
 <template>
-  <div class="demo-page">
-    <h1 style="margin: 0 auto;">
-      <a href="https://github.com/zedjarvis/vue3-compare-image" target="_blank">Vue3 Compare Image</a>
-      <VueCompareImage left-image="/favicon.ico" right-image="/favicon.ico" />
-    </h1>
+  <div class="container" style="max-width: 800px; width: auto; height: auto;">
+    <VueCompareImage left-image="/cat1.jpg" left-image-label="Left Image" right-image="/cat2.jpg" right-image-label="Right Image" vertical slider-line-color="red"/>
   </div>
 </template>
