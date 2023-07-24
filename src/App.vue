ad73071ebcdd8e9ca6b95935342fc28eb8c9753f
Cedrouseroll omondi <omondicedo@gmail.com>
2023-07-24 22:55:18 +0300

    demo tinkering

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,18 @@
 import cat1 from "/src/assets/cat1.jpg";
 import cat2 from "/src/assets/cat2.jpg";
 
+const startCallback = (pos: number) => {
+  console.log("Sliding has started ", pos)
+}
+
+const endCallback = (pos: number) => {
+  console.log("Sliding has ended ", pos)
+}
+
 </script>
 
 <template>
   <div class="container" style="max-width: 800px; width: auto; height: auto;">
-    <VueCompareImage :left-image="cat1" left-image-label="Left Image" :right-image="cat2" right-image-label="Right Image" />
+    <VueCompareImage @slide-start="startCallback" @slide-end="endCallback"  :left-image="cat1" left-image-label="Left Image" :right-image="cat2" right-image-label="Right Image" handle="<div style='border: 1px solid red'>Handle</div>" />
   </div>
 </template>
