6b78eaa469c61ec9a92befaa6d7ac60bc405410a
Cedrouseroll omondi <omondicedo@gmail.com>
2023-07-29 15:53:52 +0300

    test: added tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,6 +7,10 @@ export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
+      coverage: {
+        provider: "v8",
+        reporter: ['text', 'json', 'html'],
+      },
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/*'],
       root: fileURLToPath(new URL('./', import.meta.url)),
