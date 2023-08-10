38665002c048330866c66b33658997345f787daf
Cedrouseroll omondi <omondicedo@gmail.com>
2023-08-10 14:51:16 +0300

    refactor: no default export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-import type { DefineComponent } from "vue";
 import { VueCompareImage } from "./components";
 
 
-export default VueCompareImage as unknown as DefineComponent<typeof VueCompareImage, {}, {}>
+export { VueCompareImage }
