8dd85ce8c91349e079c467a18ffed3cb1eebc77f
Cedrouseroll omondi <omondicedo@gmail.com>
2023-08-10 14:48:37 +0300

    core: using antfu eslint code style

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,12 @@
 /* eslint-env node */
 require('@rushstack/eslint-patch/modern-module-resolution');
+const process = require('node:process')
+
+process.env.ESLINT_TSCONFIG = 'tsconfig.json'
+
 module.exports = {
   root: true,
-  'extends': ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-typescript', '@vue/eslint-config-prettier/skip-formatting', 'plugin:storybook/recommended'],
+  'extends': ['@antfu', 'plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-typescript', '@vue/eslint-config-prettier/skip-formatting', 'plugin:storybook/recommended'],
   parserOptions: {
     ecmaVersion: 'latest'
   }
