efcba968bfe4737698b02b1531f871a732bcf89a
Cedrouseroll omondi <omondicedo@gmail.com>
2023-09-01 16:31:13 +0300

    refactor: import from components folder

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
-import type { Plugin } from 'vue'
-import VueCompareImage from './components/VueCompareImage.vue'
-
-type VueCompareImageType = typeof VueCompareImage
+import type { App, Plugin } from 'vue'
+import { VueCompareImage } from './components'
 
 const install: Plugin = {
-  install(app) {
+  install(app: App) {
     app.component('VueCompareImage', VueCompareImage)
   },
 }
 
 export default install
 
-export { VueCompareImage, type VueCompareImageType }
+export { VueCompareImage }
