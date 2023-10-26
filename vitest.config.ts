c18962f5269da80ce7332cfe70b584f2d8f224d0
Cedrouseroll Omondi <omondicedo@gmail.com>
2023-10-26 19:28:20 +0300

    test: added tests

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    exclude: ['node_modules', 'dist'],
+    coverage: {
+      enabled: true,
+      provider: 'v8',
+      reporter: ['html', 'html-spa'],
+      reportsDirectory: './test/coverage',
+    },
+  },
+})
